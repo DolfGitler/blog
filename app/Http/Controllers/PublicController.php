13e2b428dc95db882e8b2d85def69932a9df4563
Devin Caith Rohtla <devincaith.rohtla@tptlive.ee>
2025-09-30 15:31:42 +0300

    add posts and logic

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function index() {
+        $page = request()->query('page', 1); // p1 => 16, p2 => 16, p3 => 32, p4 => 48, p5 => 64
+        $offset = ($page-1) * 16;
+        $posts = Post::take(16)->skip($offset)->get();
+        return view('welcome', compact('posts'));
+    }
+
+    public function page1() {
+        return view('page1');
+    }
+
+    public function page2() {
+        return view('page2');
+    }
+}
